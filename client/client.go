4927856051a8030c23eed57cb3aa80e9f5cefdac
João Victor <joao.coutinho9@gmail.com>
2019-05-22 18:12:09 -0300

    Add user registration

--- client/client.go
+++ client/client.go
@@ -1,23 +1,11 @@
 package main
 
 import (
-	"fmt"
-
 	"../../flame"
 )
 
 func main() {
-	channel, err := flame.Connect("localhost", 2020)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println("Connected!")
-		_, err = channel.InitializeTopic("topic")
-		_, err = channel.AccessTopic("topic2")
-		fmt.Println(err)
-		end := make(chan int)
-		<-end
-		//chans, _ := topicHandler.Subscribe()
-		fmt.Println("hey")
-	}
+	flame.Connect("localhost", 2020, "client1")
+	end := make(chan int)
+	<-end
 }
--- connection.go
+++ connection.go
@@ -4,13 +4,26 @@ import (
 	"errors"
 
 	"./handler"
+	"./message"
 )
 
 // Connect connects to the following host and port.
-func Connect(host string, port int) (*Channel, error) {
+func Connect(host string, port int, identifier string) (*Channel, error) {
+
 	handler := handler.New(host, port)
 	if err := handler.Connect(); err != nil { // TODO: try again.
 		return nil, errors.New("Connection could not be made, try again later")
 	}
-	return &Channel{handler}, nil
+
+	channel := &Channel{handler}
+	err := sendRegistrationRequest(identifier, channel)
+	return channel, err
+}
+
+func sendRegistrationRequest(identifier string, channel *Channel) error {
+	request := message.NewRequest(identifier, message.Register, "", nil)
+	if err := channel.call(request); err != nil {
+		return err
+	}
+	return nil
 }
